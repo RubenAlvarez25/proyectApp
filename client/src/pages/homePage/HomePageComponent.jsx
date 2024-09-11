5c53254249c75b9bd368866151170508f9bcecc5
Ruben Alvarez <rubenalvarezprofesional@gmail.com>
2024-09-11 17:31:21 +0200

    small fix

--- client/src/pages/homePage/HomePageComponent.jsx
+++ client/src/pages/homePage/HomePageComponent.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { createNew } from "../../services/news";
-import { FormComponent } from "../../../components/forms/FormComponent";
+import { FormComponent } from "../../components/forms/FormComponent";
 
-import { Alert } from 'react-bootstrap';
+import { Alert } from "react-bootstrap";
 
 import "./homePageStyle.scss";
 
@@ -11,39 +11,37 @@ export const HomePageComponent = () => {
   const navigate = useNavigate();
 
   const [showAlert, setShowAlert] = useState(false);
-  const [alertMessage, setAlertMessage] = useState('');
+  const [alertMessage, setAlertMessage] = useState("");
 
   const handleFormSubmit = (formData) => {
-
     try {
-      const response = createNew('/createNews', formData);
+      const response = createNew("/createNews", formData);
 
-      setAlertMessage('Nueva Noticia !');
+      setAlertMessage("Nueva Noticia !");
       setShowAlert(true);
-
     } catch (error) {
-      console.error('Error creating news:', error);
+      console.error("Error creating news:", error);
     }
 
-     setTimeout(() => {
+    setTimeout(() => {
       setShowAlert(false);
     }, 3000);
-  }
+  };
 
   return (
     <>
       <div className="div-ppl-home">
-      <h1>BIENVENIDO A LA PAGINA PRINCIPAL</h1>
-      <button onClick={() => navigate(`/allNews`)}>TODAS LAS NOTICIAS</button>
-      <button onClick={() => navigate(`/archived`)}>NOTICIAS ARCHIVADAS</button>
+        <h1>BIENVENIDO A LA PAGINA PRINCIPAL</h1>
+        <button onClick={() => navigate(`/allNews`)}>TODAS LAS NOTICIAS</button>
+        <button onClick={() => navigate(`/archived`)}>
+          NOTICIAS ARCHIVADAS
+        </button>
       </div>
-       {showAlert && (
-          <Alert onClose={() => setShowAlert(false)}>
-            {alertMessage}
-          </Alert>
-        )}
+      {showAlert && (
+        <Alert onClose={() => setShowAlert(false)}>{alertMessage}</Alert>
+      )}
       <div>
-        <FormComponent onSubmit={handleFormSubmit}  />
+        <FormComponent onSubmit={handleFormSubmit} />
       </div>
     </>
   );
